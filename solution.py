aaca65cb2d907c5089e3fa046384ccfb32865445
Thaliorne <130053125+Thaliorne@users.noreply.github.com>
2023-04-07 21:26:00 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,28 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 259636079 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
+    data = x
+    # Определяем функцию для оценки коэффициента x
+    def estimate_x(data):
+        # Оцениваем параметры распределения
+        mu = np.mean(np.log(data) - 307)
+        sigma = np.std(np.log(data) - 307)
+
+        # Возвращаем оценку коэффициента x
+        return np.exp(307 + sigma**2 / 2)
+
+    # Определяем функцию для bootstrap
+    def bootstrap(data, n_samples=1000, sample_size=None):
+        if sample_size is None:
+            sample_size = len(data)
+        samples = np.random.choice(data, size=(n_samples, sample_size), replace=True)
+        return samples
+
+    # Выполняем bootstrap и оцениваем коэффициент x на каждой выборке
+    samples = bootstrap(data)
+    x = np.array([estimate_x(sample) for sample in samples])
+    
     return x.mean() # Ваш ответ
