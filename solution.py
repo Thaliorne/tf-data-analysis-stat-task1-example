f5c645fd4307be93b859796dcd3833689661462b
Thaliorne <130053125+Thaliorne@users.noreply.github.com>
2023-04-08 10:43:10 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,25 +5,12 @@ import numpy as np
 chat_id = 259636079 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    data = x
-    # Определяем функцию для оценки коэффициента x
-    def estimate_x(data):
-        # Оцениваем параметры распределения
-        mu = np.mean(np.log(data) - 307)
-        sigma = np.std(np.log(data) - 307)
-
-        # Возвращаем оценку коэффициента x
-        return np.exp(307 + sigma**2 / 2)
-
-    # Определяем функцию для bootstrap
-    def bootstrap(data, n_samples=1000, sample_size=None):
-        if sample_size is None:
-            sample_size = len(data)
-        samples = np.random.choice(data, size=(n_samples, sample_size), replace=True)
-        return samples
-
-    # Выполняем bootstrap и оцениваем коэффициент x на каждой выборке
-    samples = bootstrap(data)
-    x = np.array([estimate_x(sample) for sample in samples])
-    
+  def solution(x: np.array) -> float:
+    data = x - 307
+    data = np.log(data)
+    mu_est_list = []
+    mean_mu_est_list = []
+    for i in range(10000):
+        mu_est_list.append(pd.Series(data).sample(5,replace=True).mean())      
+    x = np.array(mu_est_list)
     return x.mean() # Ваш ответ
